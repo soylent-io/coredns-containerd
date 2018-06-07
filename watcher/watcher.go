3a1602325e2110552024f31c2692cca7a61737f7
Mathieu Lecarme <mlecarme@bearstech.com>
2018-06-07 13:22:14 +0200

    Subscribe and filters

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -2,35 +2,95 @@ package watcher
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 
+	"github.com/docker/docker/api/types"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/containerd/containerd"
 	"github.com/containerd/containerd/api/events"
 	"github.com/containerd/typeurl"
+	"github.com/docker/docker/client"
 	"github.com/opencontainers/runtime-spec/specs-go"
+)
+
+// Containers events
+const (
+	TaskStart = "/task/start" // Container start
 )
 
 // Watcher watch the watchmen
 // Watch Containerd for Docker events
 type Watcher struct {
 	client *containerd.Client
+	docker *client.Client
 }
 
 // New Watcher
-func New(socket string) (*Watcher, error) {
-	client, err := containerd.New(socket, containerd.WithDefaultNamespace("moby"))
+func New(socketContainerd, socketDocker string) (*Watcher, error) {
+	cli, err := containerd.New(socketContainerd, containerd.WithDefaultNamespace("moby"))
+	if err != nil {
+		return nil, err
+	}
+	var clientDocker *client.Client
+	if socketDocker == "" {
+		clientDocker, err = client.NewEnvClient()
+	} else {
+		clientDocker, err = client.NewClient(socketDocker, "", &http.Client{}, map[string]string{})
+	}
 	if err != nil {
 		return nil, err
 	}
 	return &Watcher{
-		client: client,
+		client: cli,
+		docker: clientDocker,
 	}, nil
 }
 
 // Version of containerd
 func (w *Watcher) Version() (containerd.Version, error) {
 	return w.client.Version(context.Background())
+}
+
+// Subscribe for containers events
+func (w *Watcher) Subscribe(ctx context.Context, topic string, filters ...string) (chan *types.Container, chan error) {
+	cc := make(chan *types.Container, 1)
+	ce := make(chan error, 1)
+	ch, errs := w.client.Subscribe(ctx, "namespace==moby")
+	go func() {
+		for {
+			select {
+			case e := <-errs:
+				ce <- e
+			case c := <-ch:
+				if c.Topic != topic {
+					break
+				}
+				v, err := typeurl.UnmarshalAny(c.Event)
+				if err != nil {
+					ce <- err
+					break
+				}
+				switch c.Topic {
+				case TaskStart:
+					start, ok := v.(*events.TaskStart)
+					if !ok {
+						ce <- fmt.Errorf("Can't cast to TaskStart : %s", start)
+						break
+					}
+					cont, err := w.docker.ContainerInspect(ctx, start.ContainerID)
+					if err != nil {
+						ce <- err
+						break
+					}
+					log.Info(cont)
+				}
+
+			}
+		}
+	}()
+	return cc, ce
 }
 
 // Watch events
