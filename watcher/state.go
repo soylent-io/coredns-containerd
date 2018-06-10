ea6b83f2200917dbbfd148da1a9822e60a9d50a4
Mathieu Lecarme <mlecarme@bearstech.com>
2018-06-10 21:48:45 +0200

    Handles events as CRUD

---
+++ watcher/state.go
@@ -0,0 +1,67 @@
+package watcher
+
+import (
+	"context"
+
+	"github.com/containerd/containerd/api/events"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	log "github.com/sirupsen/logrus"
+)
+
+// State manage containers as a CRUD
+type State struct {
+	watcher    *Watcher
+	containers map[string]*types.ContainerJSON
+}
+
+// NewState returns a new State
+func NewState(socketContainerd, socketDocker string) (*State, error) {
+	w, err := New(socketContainerd, socketDocker)
+	if err != nil {
+		return nil, err
+	}
+	return &State{
+		watcher: w,
+	}, nil
+}
+
+// HandleCreate handles create events
+func (s *State) HandleCreate(filter string, handler func(*types.ContainerJSON)) error {
+	return s.watcher.HandleStart(filter, func(cont *types.ContainerJSON, event *events.TaskStart) {
+		s.containers[cont.ID] = cont
+		handler(cont)
+	})
+}
+
+// HandleDelete handles delete events
+func (s *State) HandleDelete(filter string, handler func(*types.ContainerJSON)) error {
+	return s.watcher.HandleExit(filter, func(cont *types.ContainerJSON, event *events.TaskExit) {
+		delete(s.containers, cont.ID)
+		handler(cont)
+	})
+}
+
+// Listen looks for already existing containers and watch for events
+func (s *State) Listen(ctxw context.Context) error {
+	ctx := context.Background()
+	containers, err := s.watcher.Container.Containers(ctx, "")
+	if err != nil {
+		return err
+	}
+	for _, container := range containers {
+		go func(docker *client.Client, id string) {
+			ctx := context.Background()
+			cont, err := docker.ContainerInspect(ctx, id)
+			if err != nil {
+				log.Error(err)
+				return
+			}
+			for _, h := range s.watcher.startHandlers {
+				h.handler(&cont, nil)
+			}
+		}(s.watcher.Docker, container.ID())
+	}
+	s.watcher.Listen(ctxw)
+	return nil
+}
