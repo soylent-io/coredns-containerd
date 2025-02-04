87ba970bb8b7fef3a46c5b1bc27299a59a228bbd
Maxim Khon <fjoe@samodelkin.net>
2025-02-04 18:19:25 +0000

    Remove empty AAAA response as it causes the following errors:
    ;; Got bad packet: unexpected end of input
    56 bytes
    50 f2 85 00 00 01 00 01 00 00 00 00 02 64 62 04          P............db.
    6e 6f 64 65 05 6c 6f 63 61 6c 00 00 1c 00 01 02          node.local......
    64 62 04 6e 6f 64 65 05 6c 6f 63 61 6c 00 00 1c          db.node.local...
    00 01 00 00 00 1e 00 00                                  ........

--- containerd/containerd.go
+++ containerd/containerd.go
@@ -83,20 +83,6 @@ func (cd *ContainerdDiscovery) ServeDNS(ctx context.Context, w dns.ResponseWrite
 		val, ok := cd.AAAA[state.QName()]
 		if ok {
 			answers = getAnswer(state.Name(), []net.IP{val}, cd.ttl, true)
-		} else {
-			_, ok := cd.A[state.QName()]
-			if ok {
-				// in accordance with https://tools.ietf.org/html/rfc6147#section-5.1.2 we should return an empty answer section if no AAAA records are available and a A record is available when the client requested AAAA
-				record := new(dns.AAAA)
-				record.Hdr = dns.RR_Header{
-					Name:     state.Name(),
-					Rrtype:   dns.TypeAAAA,
-					Class:    dns.ClassINET,
-					Ttl:      cd.ttl,
-					Rdlength: 0,
-				}
-				answers = append(answers, record)
-			}
 		}
 	}
 
